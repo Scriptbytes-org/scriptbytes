297dc98b706cf00c482f1715c57ab8b8e1650d2e
Scriptbytes-org <106768809+Scriptbytes-org@users.noreply.github.com>
2023-05-05 03:44:52 +0300

    Add files via upload

---
+++ scriptbytesorgcsharp.cs
@@ -0,0 +1,786 @@
+int say;
+say = 0;
+for (int i = 1; i < 100; i++)
+{
+if ((i % 8 == 0) && (i % 12 != 0))
+{
+Console.WriteLine(i);
+say++;
+
+}
+}
+Console.WriteLine(“{0}adet sayı bulundu”,say);
+Console.ReadKey();
+int say;
+say = 0;
+for (int i = 1; i < 100; i++)
+{
+if ((i % 8 == 0) && (i % 12 != 0))
+{
+Console.WriteLine(i);
+say++;
+
+}
+}
+Console.WriteLine(“{0}adet sayı bulundu”,say);
+Console.ReadKey();
+int say;
+say = 0;
+for (int i = 1; i < 100; i++)
+{
+if ((i % 8 == 0) && (i % 12 != 0))
+{
+Console.WriteLine(i);
+say++;
+
+}
+}
+Console.WriteLine(“{0}adet sayı bulundu”,say);
+Console.ReadKey();
+int say;
+say = 0;
+for (int i = 1; i < 100; i++)
+{
+if ((i % 8 == 0) && (i % 12 != 0))
+{
+Console.WriteLine(i);
+say++;
+
+}
+}
+Console.WriteLine(“{0}adet sayı bulundu”,say);
+Console.ReadKey();
+
+int say;
+say = 0;
+for (int i = 1; i < 100; i++)
+{
+if ((i % 8 == 0) && (i % 12 != 0))
+{
+Console.WriteLine(i);
+say++;
+
+}
+}
+Console.WriteLine(“{0}adet sayı bulundu”,say);
+Console.ReadKey();
+int say;
+say = 0;
+for (int i = 1; i < 100; i++)
+{
+if ((i % 8 == 0) && (i % 12 != 0))
+{
+Console.WriteLine(i);
+say++;
+
+}
+}
+Console.WriteLine(“{0}adet sayı bulundu”,say);
+Console.ReadKey();
+int say;
+say = 0;
+for (int i = 1; i < 100; i++)
+{
+if ((i % 8 == 0) && (i % 12 != 0))
+{
+Console.WriteLine(i);
+say++;
+
+}
+}
+Console.WriteLine(“{0}adet sayı bulundu”,say);
+Console.ReadKey();
+int say;
+say = 0;
+for (int i = 1; i < 100; i++)
+{
+if ((i % 8 == 0) && (i % 12 != 0))
+{
+Console.WriteLine(i);
+say++;
+
+}
+}
+Console.WriteLine(“{0}adet sayı bulundu”,say);
+Console.ReadKey();
+int say;
+say = 0;
+for (int i = 1; i < 100; i++)
+{
+if ((i % 8 == 0) && (i % 12 != 0))
+{
+Console.WriteLine(i);
+say++;
+
+}
+}
+Console.WriteLine(“{0}adet sayı bulundu”,say);
+Console.ReadKey();
+int say;
+say = 0;
+for (int i = 1; i < 100; i++)
+{
+if ((i % 8 == 0) && (i % 12 != 0))
+{
+Console.WriteLine(i);
+say++;
+
+}
+}
+Console.WriteLine(“{0}adet sayı bulundu”,say);
+Console.ReadKey();
+int say;
+say = 0;
+for (int i = 1; i < 100; i++)
+{
+if ((i % 8 == 0) && (i % 12 != 0))
+{
+Console.WriteLine(i);
+say++;
+
+}
+}
+Console.WriteLine(“{0}adet sayı bulundu”,say);
+Console.ReadKey();
+int say;
+say = 0;
+for (int i = 1; i < 100; i++)
+{
+if ((i % 8 == 0) && (i % 12 != 0))
+{
+Console.WriteLine(i);
+say++;
+
+}
+}
+Console.WriteLine(“{0}adet sayı bulundu”,say);
+Console.ReadKey();
+int say;
+say = 0;
+for (int i = 1; i < 100; i++)
+{
+if ((i % 8 == 0) && (i % 12 != 0))
+{
+Console.WriteLine(i);
+say++;
+
+}
+}
+Console.WriteLine(“{0}adet sayı bulundu”,say);
+Console.ReadKey();
+int say;
+say = 0;
+for (int i = 1; i < 100; i++)
+{
+if ((i % 8 == 0) && (i % 12 != 0))
+{
+Console.WriteLine(i);
+say++;
+
+}
+}
+Console.WriteLine(“{0}adet sayı bulundu”,say);
+Console.ReadKey();
+
+int say;
+say = 0;
+for (int i = 1; i < 100; i++)
+{
+if ((i % 8 == 0) && (i % 12 != 0))
+{
+Console.WriteLine(i);
+say++;
+
+}
+}
+Console.WriteLine(“{0}adet sayı bulundu”,say);
+Console.ReadKey();
+int say;
+say = 0;
+for (int i = 1; i < 100; i++)
+{
+if ((i % 8 == 0) && (i % 12 != 0))
+{
+Console.WriteLine(i);
+say++;
+
+}
+}
+Console.WriteLine(“{0}adet sayı bulundu”,say);
+Console.ReadKey();
+int say;
+say = 0;
+for (int i = 1; i < 100; i++)
+{
+if ((i % 8 == 0) && (i % 12 != 0))
+{
+Console.WriteLine(i);
+say++;
+
+}
+}
+Console.WriteLine(“{0}adet sayı bulundu”,say);
+Console.ReadKey();
+int say;
+say = 0;
+for (int i = 1; i < 100; i++)
+{
+if ((i % 8 == 0) && (i % 12 != 0))
+{
+Console.WriteLine(i);
+say++;
+
+}
+}
+Console.WriteLine(“{0}adet sayı bulundu”,say);
+Console.ReadKey();
+int say;
+say = 0;
+for (int i = 1; i < 100; i++)
+{
+if ((i % 8 == 0) && (i % 12 != 0))
+{
+Console.WriteLine(i);
+say++;
+
+}
+}
+Console.WriteLine(“{0}adet sayı bulundu”,say);
+Console.ReadKey();
+int say;
+say = 0;
+for (int i = 1; i < 100; i++)
+{
+if ((i % 8 == 0) && (i % 12 != 0))
+{
+Console.WriteLine(i);
+say++;
+
+}
+}
+Console.WriteLine(“{0}adet sayı bulundu”,say);
+Console.ReadKey();
+int say;
+say = 0;
+for (int i = 1; i < 100; i++)
+{
+if ((i % 8 == 0) && (i % 12 != 0))
+{
+Console.WriteLine(i);
+say++;
+
+}
+}
+Console.WriteLine(“{0}adet sayı bulundu”,say);
+Console.ReadKey();
+int say;
+say = 0;
+for (int i = 1; i < 100; i++)
+{
+if ((i % 8 == 0) && (i % 12 != 0))
+{
+Console.WriteLine(i);
+say++;
+
+}
+}
+Console.WriteLine(“{0}adet sayı bulundu”,say);
+Console.ReadKey();
+int say;
+say = 0;
+for (int i = 1; i < 100; i++)
+{
+if ((i % 8 == 0) && (i % 12 != 0))
+{
+Console.WriteLine(i);
+say++;
+
+}
+}
+Console.WriteLine(“{0}adet sayı bulundu”,say);
+Console.ReadKey();
+int say;
+say = 0;
+for (int i = 1; i < 100; i++)
+{
+if ((i % 8 == 0) && (i % 12 != 0))
+{
+Console.WriteLine(i);
+say++;
+
+}
+}
+Console.WriteLine(“{0}adet sayı bulundu”,say);
+Console.ReadKey();
+
+int say;
+say = 0;
+for (int i = 1; i < 100; i++)
+{
+if ((i % 8 == 0) && (i % 12 != 0))
+{
+Console.WriteLine(i);
+say++;
+
+}
+}
+Console.WriteLine(“{0}adet sayı bulundu”,say);
+Console.ReadKey();
+int say;
+say = 0;
+for (int i = 1; i < 100; i++)
+{
+if ((i % 8 == 0) && (i % 12 != 0))
+{
+Console.WriteLine(i);
+say++;
+
+}
+}
+Console.WriteLine(“{0}adet sayı bulundu”,say);
+Console.ReadKey();
+int say;
+say = 0;
+for (int i = 1; i < 100; i++)
+{
+if ((i % 8 == 0) && (i % 12 != 0))
+{
+Console.WriteLine(i);
+say++;
+
+}
+}
+Console.WriteLine(“{0}adet sayı bulundu”,say);
+Console.ReadKey();
+int say;
+say = 0;
+for (int i = 1; i < 100; i++)
+{
+if ((i % 8 == 0) && (i % 12 != 0))
+{
+Console.WriteLine(i);
+say++;
+
+}
+}
+Console.WriteLine(“{0}adet sayı bulundu”,say);
+Console.ReadKey();
+int say;
+say = 0;
+for (int i = 1; i < 100; i++)
+{
+if ((i % 8 == 0) && (i % 12 != 0))
+{
+Console.WriteLine(i);
+say++;
+
+}
+}
+Console.WriteLine(“{0}adet sayı bulundu”,say);
+Console.ReadKey();
+int say;
+say = 0;
+for (int i = 1; i < 100; i++)
+{
+if ((i % 8 == 0) && (i % 12 != 0))
+{
+Console.WriteLine(i);
+say++;
+
+}
+}
+Console.WriteLine(“{0}adet sayı bulundu”,say);
+Console.ReadKey();
+int say;
+say = 0;
+for (int i = 1; i < 100; i++)
+{
+if ((i % 8 == 0) && (i % 12 != 0))
+{
+Console.WriteLine(i);
+say++;
+
+}
+}
+Console.WriteLine(“{0}adet sayı bulundu”,say);
+Console.ReadKey();
+int say;
+say = 0;
+for (int i = 1; i < 100; i++)
+{
+if ((i % 8 == 0) && (i % 12 != 0))
+{
+Console.WriteLine(i);
+say++;
+
+}
+}
+Console.WriteLine(“{0}adet sayı bulundu”,say);
+Console.ReadKey();
+int say;
+say = 0;
+for (int i = 1; i < 100; i++)
+{
+if ((i % 8 == 0) && (i % 12 != 0))
+{
+Console.WriteLine(i);
+say++;
+
+}
+}
+Console.WriteLine(“{0}adet sayı bulundu”,say);
+Console.ReadKey();
+int say;
+say = 0;
+for (int i = 1; i < 100; i++)
+{
+if ((i % 8 == 0) && (i % 12 != 0))
+{
+Console.WriteLine(i);
+say++;
+
+}
+}
+Console.WriteLine(“{0}adet sayı bulundu”,say);
+Console.ReadKey();
+
+int say;
+say = 0;
+for (int i = 1; i < 100; i++)
+{
+if ((i % 8 == 0) && (i % 12 != 0))
+{
+Console.WriteLine(i);
+say++;
+
+}
+}
+Console.WriteLine(“{0}adet sayı bulundu”,say);
+Console.ReadKey();
+int say;
+say = 0;
+for (int i = 1; i < 100; i++)
+{
+if ((i % 8 == 0) && (i % 12 != 0))
+{
+Console.WriteLine(i);
+say++;
+
+}
+}
+Console.WriteLine(“{0}adet sayı bulundu”,say);
+Console.ReadKey();
+int say;
+say = 0;
+for (int i = 1; i < 100; i++)
+{
+if ((i % 8 == 0) && (i % 12 != 0))
+{
+Console.WriteLine(i);
+say++;
+
+}
+}
+Console.WriteLine(“{0}adet sayı bulundu”,say);
+Console.ReadKey();
+int say;
+say = 0;
+for (int i = 1; i < 100; i++)
+{
+if ((i % 8 == 0) && (i % 12 != 0))
+{
+Console.WriteLine(i);
+say++;
+
+}
+}
+Console.WriteLine(“{0}adet sayı bulundu”,say);
+Console.ReadKey();
+int say;
+say = 0;
+for (int i = 1; i < 100; i++)
+{
+if ((i % 8 == 0) && (i % 12 != 0))
+{
+Console.WriteLine(i);
+say++;
+
+}
+}
+Console.WriteLine(“{0}adet sayı bulundu”,say);
+Console.ReadKey();
+int say;
+say = 0;
+for (int i = 1; i < 100; i++)
+{
+if ((i % 8 == 0) && (i % 12 != 0))
+{
+Console.WriteLine(i);
+say++;
+
+}
+}
+Console.WriteLine(“{0}adet sayı bulundu”,say);
+Console.ReadKey();
+int say;
+say = 0;
+for (int i = 1; i < 100; i++)
+{
+if ((i % 8 == 0) && (i % 12 != 0))
+{
+Console.WriteLine(i);
+say++;
+
+}
+}
+Console.WriteLine(“{0}adet sayı bulundu”,say);
+Console.ReadKey();
+int say;
+say = 0;
+for (int i = 1; i < 100; i++)
+{
+if ((i % 8 == 0) && (i % 12 != 0))
+{
+Console.WriteLine(i);
+say++;
+
+}
+}
+Console.WriteLine(“{0}adet sayı bulundu”,say);
+Console.ReadKey();
+int say;
+say = 0;
+for (int i = 1; i < 100; i++)
+{
+if ((i % 8 == 0) && (i % 12 != 0))
+{
+Console.WriteLine(i);
+say++;
+
+}
+}
+Console.WriteLine(“{0}adet sayı bulundu”,say);
+Console.ReadKey();
+int say;
+say = 0;
+for (int i = 1; i < 100; i++)
+{
+if ((i % 8 == 0) && (i % 12 != 0))
+{
+Console.WriteLine(i);
+say++;
+
+}
+}
+Console.WriteLine(“{0}adet sayı bulundu”,say);
+Console.ReadKey();
+
+int say;
+say = 0;
+for (int i = 1; i < 100; i++)
+{
+if ((i % 8 == 0) && (i % 12 != 0))
+{
+Console.WriteLine(i);
+say++;
+
+}
+}
+Console.WriteLine(“{0}adet sayı bulundu”,say);
+Console.ReadKey();
+int say;
+say = 0;
+for (int i = 1; i < 100; i++)
+{
+if ((i % 8 == 0) && (i % 12 != 0))
+{
+Console.WriteLine(i);
+say++;
+
+}
+}
+Console.WriteLine(“{0}adet sayı bulundu”,say);
+Console.ReadKey();
+int say;
+say = 0;
+for (int i = 1; i < 100; i++)
+{
+if ((i % 8 == 0) && (i % 12 != 0))
+{
+Console.WriteLine(i);
+say++;
+
+}
+}
+Console.WriteLine(“{0}adet sayı bulundu”,say);
+Console.ReadKey();
+int say;
+say = 0;
+for (int i = 1; i < 100; i++)
+{
+if ((i % 8 == 0) && (i % 12 != 0))
+{
+Console.WriteLine(i);
+say++;
+
+}
+}
+Console.WriteLine(“{0}adet sayı bulundu”,say);
+Console.ReadKey();
+int say;
+say = 0;
+for (int i = 1; i < 100; i++)
+{
+if ((i % 8 == 0) && (i % 12 != 0))
+{
+Console.WriteLine(i);
+say++;
+
+}
+}
+Console.WriteLine(“{0}adet sayı bulundu”,say);
+Console.ReadKey();
+int say;
+say = 0;
+for (int i = 1; i < 100; i++)
+{
+if ((i % 8 == 0) && (i % 12 != 0))
+{
+Console.WriteLine(i);
+say++;
+
+}
+}
+Console.WriteLine(“{0}adet sayı bulundu”,say);
+Console.ReadKey();
+int say;
+say = 0;
+for (int i = 1; i < 100; i++)
+{
+if ((i % 8 == 0) && (i % 12 != 0))
+{
+Console.WriteLine(i);
+say++;
+
+}
+}
+Console.WriteLine(“{0}adet sayı bulundu”,say);
+Console.ReadKey();
+int say;
+say = 0;
+for (int i = 1; i < 100; i++)
+{
+if ((i % 8 == 0) && (i % 12 != 0))
+{
+Console.WriteLine(i);
+say++;
+
+}
+}
+Console.WriteLine(“{0}adet sayı bulundu”,say);
+Console.ReadKey();
+int say;
+say = 0;
+for (int i = 1; i < 100; i++)
+{
+if ((i % 8 == 0) && (i % 12 != 0))
+{
+Console.WriteLine(i);
+say++;
+
+}
+}
+Console.WriteLine(“{0}adet sayı bulundu”,say);
+Console.ReadKey();
+int say;
+say = 0;
+for (int i = 1; i < 100; i++)
+{
+if ((i % 8 == 0) && (i % 12 != 0))
+{
+Console.WriteLine(i);
+say++;
+
+}
+}
+Console.WriteLine(“{0}adet sayı bulundu”,say);
+Console.ReadKey();
+
+int say;
+say = 0;
+for (int i = 1; i < 100; i++)
+{
+if ((i % 8 == 0) && (i % 12 != 0))
+{
+Console.WriteLine(i);
+say++;
+
+}
+}
+Console.WriteLine(“{0}adet sayı bulundu”,say);
+Console.ReadKey();
+int say;
+say = 0;
+for (int i = 1; i < 100; i++)
+{
+if ((i % 8 == 0) && (i % 12 != 0))
+{
+Console.WriteLine(i);
+say++;
+
+}
+}
+Console.WriteLine(“{0}adet sayı bulundu”,say);
+Console.ReadKey();
+int say;
+say = 0;
+for (int i = 1; i < 100; i++)
+{
+if ((i % 8 == 0) && (i % 12 != 0))
+{
+Console.WriteLine(i);
+say++;
+
+}
+}
+Console.WriteLine(“{0}adet sayı bulundu”,say);
+Console.ReadKey();
+int say;
+say = 0;
+for (int i = 1; i < 100; i++)
+{
+if ((i % 8 == 0) && (i % 12 != 0))
+{
+Console.WriteLine(i);
+say++;
+
+}
+}
+Console.WriteLine(“{0}adet sayı bulundu”,say);
+Console.ReadKey();
+int say;
+say = 0;
+for (int i = 1; i < 100; i++)
+{
+if ((i % 8 == 0) && (i % 12 != 0))
+{
+Console.WriteLine(i);
+say++;
+
+}
+}
+Console.WriteLine(“{0}adet sayı bulundu”,say);
+Console.ReadKey();
+int say;
+say = 0;
+for (int i = 1; i < 100; i++)
+{
+if ((i % 8 == 0) && (i % 12 != 0))
+{
+Console.WriteLine(i);
+say++;
+
+}
+}
+Console.WriteLine(“{0}adet sayı bulundu”,say);
+Console.ReadKey();
